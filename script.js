e8c3dd2e0e9b46580098e98ac537820bb989cc54
xNico013 <73437011+xNico013@users.noreply.github.com>
2023-08-21 14:09:51 +0200

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const grid = document.getElementById("grid");
-
 const items = [
     "Scripting.png",
     "image2.jpg",
     "image3.jpg",
-    "video1.mp4", // Video file is in the same directory
+    "video1.mp4",
     "video2.mp4",
     "image4.jpg",
     "image5.jpg",
@@ -26,6 +25,15 @@ items.forEach(item => {
         video.controls = true;
         gridItem.appendChild(video);
     }
+
+    gridItem.addEventListener("transitionend", () => {
+        gridItem.style.transition = "none"; // Temporarily remove transition to reset position instantly
+        gridItem.style.transform = "translateX(100%)"; // Move item to the right side
+        setTimeout(() => {
+            gridItem.style.transition = ""; // Restore transition
+            gridItem.style.transform = "translateX(0)"; // Reset item position for the next cycle
+        }, 0);
+    });
 
     grid.appendChild(gridItem);
 });
