06f084ccf6882948dbd8b9cdc13edda61e7cb05a
xNico013 <73437011+xNico013@users.noreply.github.com>
2023-08-22 11:25:45 +0200

    script.js

--- script.js
+++ script.js
@@ -31,34 +31,3 @@ items.forEach(item => {
 });
 
 
-const grid2 = document.getElementById("grid2");
-
-const items = [
-    "Scripting.png",
-    "image2.jpg",
-    "image3.jpg",
-    "video1.mp4", // Video file is in the same directory
-    "video2.mp4",
-    "image4.jpg",
-    "image5.jpg",
-    "image6.jpg",
-    "image7.jpg",
-];
-
-items.forEach(item => {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-
-    if (item.endsWith(".jpg") || item.endsWith(".png")) {
-        const image = document.createElement("img");
-        image.src = item;
-        gridItem.appendChild(image);
-    } else if (item.endsWith(".mp4")) {
-        const video = document.createElement("video");
-        video.src = item;
-        video.controls = true;
-        gridItem.appendChild(video);
-    }
-
-    grid2.appendChild(gridItem);
-});
