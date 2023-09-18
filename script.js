8c97903bfc77105736f523e515c0e7978355a57c
xNico013 <73437011+xNico013@users.noreply.github.com>
2023-09-18 23:24:39 +0200

    script.js

--- script.js
+++ script.js
@@ -38,9 +38,9 @@ const canvas = document.getElementById("sphereCanvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
-canvas.height = 600;
+canvas.height = 300;
 
-const sphereRadius = 200;
+const sphereRadius = 80;
 const sphereCenterX = canvas.width / 2;
 const sphereCenterY = canvas.height / 2;
 const dotCount = 300;
@@ -57,6 +57,14 @@ function drawDot(x, y) {
     ctx.fill();
 }
 
+function getRandomVelocity() {
+    return (Math.random() - 0.5) * maxSpeed;
+}
+
+function getRandomDirection() {
+    return Math.random() * Math.PI * 2;
+}
+
 function getRandomSphereCoordinates() {
     const u = Math.random() * 2 - 1; // Random number between -1 and 1
     const theta = Math.random() * Math.PI * 2; // Random angle between 0 and 2*PI
@@ -64,8 +72,10 @@ function getRandomSphereCoordinates() {
     const x = sphereCenterX + sphereRadius * Math.sqrt(1 - u * u) * Math.cos(theta);
     const y = sphereCenterY + sphereRadius * Math.sqrt(1 - u * u) * Math.sin(theta);
     
-    const dx = (Math.random() - 0.5) * maxSpeed;
-    const dy = (Math.random() - 0.5) * maxSpeed;
+    const speed = maxSpeed; // Constant speed
+    const direction = getRandomDirection();
+    const dx = speed * Math.cos(direction);
+    const dy = speed * Math.sin(direction);
 
     return { x, y, dx, dy };
 }
@@ -83,17 +93,17 @@ function animate() {
         dot.x += dot.dx;
         dot.y += dot.dy;
 
-        // Bounce off the sphere's border
+        // Check if the dot hits the sphere's border
         const distanceToCenter = Math.sqrt((dot.x - sphereCenterX) ** 2 + (dot.y - sphereCenterY) ** 2);
         if (distanceToCenter > sphereRadius) {
-            const angle = Math.atan2(dot.y - sphereCenterY, dot.x - sphereCenterX);
-            dot.dx *= -1; // Reverse the horizontal velocity
-            dot.dy *= -1; // Reverse the vertical velocity
-            // Move dot back to the sphere's border
-            dot.x = sphereCenterX + sphereRadius * Math.cos(angle);
-            dot.y = sphereCenterY + sphereRadius * Math.sin(angle);
+            // Bounce off the sphere's border with a new random direction and speed
+            const newDirection = getRandomDirection();
+            const newSpeed = Math.random() * maxSpeed;
+            dot.dx = newSpeed * Math.cos(newDirection);
+            dot.dy = newSpeed * Math.sin(newDirection);
         }
     }
 }
 
 animate();
+
