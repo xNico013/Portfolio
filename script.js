c4c5610778ff0b5cb4750c46c46b50040a4bf652
xNico013 <73437011+xNico013@users.noreply.github.com>
2023-10-24 17:13:14 +0200

    script.js

--- script.js
+++ script.js
@@ -44,3 +44,24 @@ const initOverlayCard = (cardEl) => {
 
 cards.forEach(initOverlayCard);
 document.body.addEventListener("pointermove", applyOverlayMask);
+
+
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+const textElements = gsap.utils.toArray('.text');
+
+textElements.forEach(text => {
+  gsap.to(text, {
+    backgroundSize: '100%',
+    ease: 'none',
+    scrollTrigger: {
+      trigger: text,
+      start: 'center 10%',
+      end: 'center 0.01%',
+      scrub: true,
+    },
+  });
+});
